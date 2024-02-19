e5ee47d63a392dd51c6b723679ec1b27599670c2
Agudey <agudeydaniel8@gmail.com>
2024-02-19 15:50:44 +0000

    testing python setup

---
+++ Python Programming/test.py
@@ -0,0 +1,8 @@
+print('Hello')
+name = input('Please, what is your name:\n')
+
+print(f'nice to meet you {name}')
+
+age = int(input('Can you please tell me your age too:\n'))
+
+print(f'next year you will be {age + 1}')
