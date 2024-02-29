02e1a9d29bd90b82ad9c9ea5049a8a35e676a689
Agudey <agudeydaniel8@gmail.com>
2024-02-29 15:01:07 +0000

    opposite stack

---
+++ Python Programming/User_defined_data_structures/stack_top_at_beginning.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        if self.items == 0:
+            return True
+
+    def push(self,item):
+        return self.items.insert(0,item)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.item)
+
+test = Stack()
+test.isEmpty()
+test.push('hello')
+test.push(4.6)
+test.push(45)
+print(test.peek())
+print(test.pop())
