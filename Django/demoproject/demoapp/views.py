3f9faa468d8cfde0bee8fd6dc3ad9aa9eac0f09a
Agudey <agudeydaniel8@gmail.com>
2024-03-09 08:41:53 +0000

    change index to home

--- Django/demoproject/demoapp/views.py
+++ Django/demoproject/demoapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. This is the index view of the demoapp")
+# def index(request):
+#     return HttpResponse("Hello, world. This is the index view of the demoapp")
 
+def home(request):
+    return HttpResponse('Welcome to Little Lemon restaurant')
