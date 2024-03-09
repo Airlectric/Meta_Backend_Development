19b529552529731b5136655c4381847d1c0171cf
Agudey <agudeydaniel8@gmail.com>
2024-03-09 11:10:39 +0000

    'drinks'

--- Django/demoproject/demoapp/views.py
+++ Django/demoproject/demoapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# def index(request):
-#     return HttpResponse("Hello, world. This is the index view of the demoapp")
+def index(request):
+    return HttpResponse("Hello, world. This is the index view of the demoapp")
 
 def home(request):
     return HttpResponse('Welcome to Little Lemon restaurant')
+
+def drink(request,drink_name):
+    drinks = {
+        'mocha' : 'coffee',
+        'tea' : 'beverage',
+        'coke' : 'soft drink'
+    }
+
+    choice_of_drink = drinks.get(drink_name, 'Unknown Drink')
+    return HttpResponse(f'<h2>{drink_name}</h2> {choice_of_drink}')
--- Django/demoproject/demoproject/urls.py
+++ Django/demoproject/demoproject/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+import demoapp
 
 urlpatterns = [
-    path('',include('demoapp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('demoapp.urls'))
 ]
