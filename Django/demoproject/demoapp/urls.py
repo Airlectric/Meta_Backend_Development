f16211e76f34fe62541609326fc7aabf0a9ace3c
Agudey <agudeydaniel8@gmail.com>
2024-03-10 22:44:50 +0000

    using namespace for differentiation

--- Django/demoproject/demoapp/urls.py
+++ Django/demoproject/demoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'demoapp'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.home, name='home'),
+    path('index', views.index, name='index'),
+    path('home', views.home, name='home'),
     path('drinks/<str:drink_name>',views.drink, name='drink')
 ]
--- Django/demoproject/demoproject/urls.py
+++ Django/demoproject/demoproject/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import include,path
 import demoapp
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('demoapp.urls'))
+    path('demo', include('demoapp.urls'))
 ]
